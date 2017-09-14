a592261e6abca93bef6fa11eac8cae3643aca5b7
Ghizmilth <hidanerferrer@gmail.com>
2017-09-14 09:40:28 -0700

    Created a map generated from another .js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,9 @@
+// var start_lat = 37.77,
+//   start_lon = -122.447,
+//   end_lat = 37.768,
+//   end_lon = -122.511;
+//const map = require("./map.js");
+
 $(document).ready(function() {
   console.log("JS is loaded");
 
@@ -18,39 +24,3 @@ $(document).ready(function() {
 // function renderRoutes(data) {
 //   console.log("This is just a test");
 // }
-
-function initMap() {
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  var directionsService = new google.maps.DirectionsService();
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
-    center: { lat: 37.77, lng: -122.447 }
-  });
-  directionsDisplay.setMap(map);
-
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-  document.getElementById("mode").addEventListener("change", function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  });
-}
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  var selectedMode = document.getElementById("mode").value;
-  directionsService.route(
-    {
-      origin: { lat: 37.77, lng: -122.447 }, // Haight.
-      destination: { lat: 37.768, lng: -122.511 }, // Ocean Beach.
-      // Note that Javascript allows us to access the constant
-      // using square brackets and a string value as its
-      // "property."
-      travelMode: google.maps.TravelMode[selectedMode]
-    },
-    function(response, status) {
-      if (status == "OK") {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
-}
