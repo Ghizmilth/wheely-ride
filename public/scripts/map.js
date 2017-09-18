b25428b8e3563dc731bdedf75859e3c9313e926a
Ghizmilth <hidanerferrer@gmail.com>
2017-09-17 19:26:25 -0700

    Show maps on page with specific information, also the list of routes available

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -47,58 +47,104 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 //render map on HTML
 function renderExplore(route) {
   console.log("Map second part initiaded");
+  renderOneMap(route);
   console.log(route);
   route.forEach(function(route) {
     renderRouteList(route);
   });
 }
 
+function renderOneMap(route) {
+  var routeTitleHtml = `
+  <!-- Page Content -->
+     <div class="container map-box">
+
+       <!-- Portfolio Item Heading -->
+       <h1 class="my-4">${route[0].route_name}
+         <small></small>
+       </h1>`;
+
+  var routeContentHtml = `<div class="col-md-4">
+    <h3 class="my-3">Route Info</h3>
+    <p></p>
+    <h3 class="my-3"></h3>
+    <ul>
+      <li><strong>Miles:</strong> ${route[0].miles} miles</li>
+      <li><strong>Climbing Feet:</strong> ${route[0].climbing_ft} ft.</li>
+      <li><strong>Pros:</strong> ${route[0].pros}</li>
+      <li><strong>Cons:</strong> ${route[0].cons}</li>
+    </ul>
+  </div>`;
+
+  start_lat = `${route[0].start_lat}`;
+  start_lon = `${route[0].start_lon}`;
+  end_lat = `${route[0].end_lat}`;
+  end_lon = `${route[0].end_lon}`;
+
+  initMap();
+
+  $("#map-title").after(routeTitleHtml);
+  $("#map-content").after(routeContentHtml);
+}
+
 function renderRouteList(route) {
   console.log("getting ready for the list");
-  var routeHtml = `
-        <div class="row city" data-city-id="${route._id}">
 
-        <div class="row"
-          <div class="col-md-3 col-sx-12 thumbnail city-photo" id="city-image">
-            <img src="${route.route_name}"></img>
-          </div>
-          <div class="col-md-12" id='style-city'>
-            <h2>${route.pros}</h2>
-            <p>${route.cons}</P>
-          </div>
+  var otherRoutesHtml = `<div class="col-md-3 col-sm-6 mb-4">
+    <a href="#">
+      <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+    </a>
+    <ul>
+    <li><strong>Route Name:</stong> ${route.route_name}</li>
+    <li><strong>Route Length:</stong> ${route.miles}</li>
+  </div>
+  `;
 
-          </div>
+  $("#other-routes").after(otherRoutesHtml);
+}
 
-          <div class="city-info">
-            <div class="row">
-              <div class="col-md-12" id="city-facts">
-                <ul id="edit-list">
-                  <li id="coordinatesInfo">Coordinates: ${route.miles}</li>
-                  <li id="cityPopulation">Population: ${route.climbing_ft}</li>
-                  <li id="cityArea">City Area: ${route.miles}</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="edition-button">
-          <button type="button" class="btn edit-city" data-city-id="${route._id}">Update City</button>
-          </div>
+function initMapTwo() {
+  console.log("map is initiated");
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
+  var map = new google.maps.Map(document.getElementById("map1"), {
+    zoom: 12,
+    center: { lat: 37.8054, lng: -122.3401 }
+  });
+  directionsDisplay.setMap(map);
 
-          <hr/>
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  document.getElementById("mode").addEventListener("change", function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+}
 
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var selectedMode = document.getElementById("mode").value;
+  directionsService.route(
+    {
+      // origin: { lat: 37.77, lng: -122.447 }, // Haight.
+      // destination: { lat: 37.768, lng: -122.511 }, // Ocean Beach.
+      origin: {
+        lat: parseFloat(start_lat),
+        lng: parseFloat(start_lon)
+      }, // Haight.
+      destination: {
+        lat: parseFloat(end_lat),
+        lng: parseFloat(end_lon)
+      }, // Ocean Beach.
 
-            <h3>Landmarks</h3>
-            <div class="row" id="clear-this-also">
-              <button type="button" class="btn btn-add-landmark add-this-landmark">Add Landmark</button>
-            </div>
-
-            <hr>
-
-          <div id="landmarks-render">
-
-          </div>
-        </div>
-        `;
-  $("#routeInfo").prepend(routeHtml);
-  //initMap();
+      // Note that Javascript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      travelMode: google.maps.TravelMode[selectedMode]
+    },
+    function(response, status) {
+      if (status == "OK") {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
 }
